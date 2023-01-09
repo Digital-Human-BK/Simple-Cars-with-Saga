9ad1e1bfbb30e556f0e7776f7ccf7e87d8027ea4
Biser Karadzhov <85568905+Digital-Human-BK@users.noreply.github.com>
2023-01-09 16:12:17 +0200

    [add] add rules to eslingrc.json

--- src/common/Toast/Toast.tsx
+++ src/common/Toast/Toast.tsx
@@ -10,7 +10,6 @@ const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
   props,
   ref
 ) {
-  // eslint-disable-next-line react/jsx-props-no-spreading
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
