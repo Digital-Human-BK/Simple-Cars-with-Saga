a2607e65be85cfc5277da546858317dc4dc305d4
Biser Karadzhov <85568905+Digital-Human-BK@users.noreply.github.com>
2023-01-10 21:49:13 +0200

    [add] added auth reducer and action types

---
+++ src/reducers/authReducer.ts
@@ -0,0 +1,72 @@
+import { AnyAction } from 'redux';
+
+import {
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
+} from '../containers/SignIn/types';
+
+import {
+  SIGNUP_REQUEST,
+  SIGNUP_SUCCESS,
+  SIGNUP_FAILURE,
+} from '../containers/SignUp/types';
+
+import { UserState } from '../interfaces/User';
+
+const initialState: UserState = {
+  userData: null,
+  loading: false,
+  error: null,
+};
+
+function authReducer(state = initialState, action: AnyAction) {
+  switch (action.type) {
+    // ========================
+    // REGISTER
+    // ========================
+    case SIGNUP_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case SIGNUP_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        error: null,
+      };
+    case SIGNUP_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+
+    // ========================
+    // LOGIN
+    // ========================
+    case LOGIN_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case LOGIN_SUCCESS:
+      return {
+        userData: action.payload,
+        loading: false,
+        error: null,
+      };
+    case LOGIN_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+
+    default:
+      return initialState;
+  }
+}
+
+export default authReducer;
