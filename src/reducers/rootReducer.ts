82ab398c3c57ab6c8006eb5ea755d70fbecd0700
Biser Karadzhov <85568905+Digital-Human-BK@users.noreply.github.com>
2023-01-11 21:17:23 +0200

    [add] added catalogReduserto the rootReduser

--- src/reducers/rootReducer.ts
+++ src/reducers/rootReducer.ts
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
 import redirectReducer from './redirectReducer';
 import authReducer from './authReducer';
+import catalogReducer from './catalogReducer';
 
 // TODO:Import reducers
 
 const rootReducer = combineReducers({
   // TODO: set reducers
   auth: authReducer,
+  catalog: catalogReducer,
   redirect: redirectReducer,
 });
 
