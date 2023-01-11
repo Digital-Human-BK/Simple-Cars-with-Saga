8c37ac59e710f1aa45d2fc93c29334f6ce5d4481
Biser Karadzhov <85568905+Digital-Human-BK@users.noreply.github.com>
2023-01-11 18:49:22 +0200

    [add] added signUpSaga into the rootSaga

--- src/sagas/rootSaga.ts
+++ src/sagas/rootSaga.ts
@@ -2,14 +2,17 @@ import { all } from 'redux-saga/effects';
 // Saga
 // **** TODO: IMPORT SAGA FILES
 import loginSaga from '../containers/SignIn/sagas';
+import signUpSaga from '../containers/SignUp/saga';
 // API
 // **** TODO: IMPORT API
 import loginApi from '../api/carsBackend/loginApi';
+import signUpApi from '../api/carsBackend/signUpApi';
 
 function* rootSaga() {
   yield all([
     // TODO: connect saga files with api
     loginSaga(loginApi),
+    signUpSaga(signUpApi),
   ]);
 }
 
