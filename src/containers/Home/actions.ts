14c40f4bbece7b6f1ecc828fdbd92dee0bd15f51
Biser Karadzhov <85568905+Digital-Human-BK@users.noreply.github.com>
2023-01-12 09:10:29 +0200

    [add] added types and actions

--- src/containers/Home/actions.ts
+++ src/containers/Home/actions.ts
@@ -1,20 +1,21 @@
-import { Car } from '../../interfaces/Car';
+import { Car, NewCar } from '../../interfaces/Car';
 
 import {
   GET_CARS_REQUEST,
   GET_CARS_SUCCESS,
   GET_CARS_ERROR,
-  // REMOVE_CAR_REQUEST,
-  // REMOVE_CAR_SUCCESS,
-  // REMOVE_CAR_ERROR,
+  CREATE_CAR_REQUEST,
+  CREATE_CAR_SUCCESS,
+  CREATE_CAR_ERROR,
   // EDIT_CAR_REQUEST,
   // EDIT_CAR_SUCCESS,
   // EDIT_CAR_ERROR,
-  // CREATE_CAR_REQUEST,
-  // CREATE_CAR_SUCCESS,
-  // CREATE_CAR_ERROR,
+  // REMOVE_CAR_REQUEST,
+  // REMOVE_CAR_SUCCESS,
+  // REMOVE_CAR_ERROR,
 } from './types';
 
+// get cars
 export function getCars() {
   return {
     type: GET_CARS_REQUEST,
@@ -31,6 +32,28 @@ export function getCarsSuccess(payload: Car[]) {
 export function getCarsError(payload: string) {
   return {
     type: GET_CARS_ERROR,
+    payload,
+  };
+}
+
+// create car
+export function createCarRequest(payload: NewCar) {
+  return {
+    type: CREATE_CAR_REQUEST,
+    payload,
+  };
+}
+
+export function createCarSuccess(payload: Car) {
+  return {
+    type: CREATE_CAR_SUCCESS,
+    payload,
+  };
+}
+
+export function createCarError(payload: string) {
+  return {
+    type: CREATE_CAR_ERROR,
     payload,
   };
 }
